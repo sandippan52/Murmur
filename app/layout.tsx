e50a880bcb0adc210a3ef9b5c543cdc146e3b5ae
sandippan52 <sandippan52@gmail.com>
2026-08-20 15:03:49 +0530

    make background black

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
+   <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
